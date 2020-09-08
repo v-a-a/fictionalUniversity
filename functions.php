ef4222b8ad69bf53d8f7f775f32c82cbc021cfbf
Vladimir <al.moody.2020@gmail.com>
2020-09-08 23:01:18 +0300

    js and css added

--- functions.php
+++ functions.php
@@ -1,8 +1,9 @@
 <?php
 function antonidis_files(){
+wp_enqueue_script( 'main-university-js', get_theme_file_uri('js/scripts-bundled.js'), null,microtime(), true );
 wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i' );
 wp_enqueue_style( 'font_awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
-wp_enqueue_style( 'antonidis_main_styles', get_stylesheet_uri() );
+wp_enqueue_style( 'antonidis_main_styles', get_stylesheet_uri(), null, microtime() );
 }
 
 add_action( 'wp_enqueue_scripts', 'antonidis_files');
