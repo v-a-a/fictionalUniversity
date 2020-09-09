773e12151fae19ff4fe85d897e8cd0dba1bbdc4b
Vladimir <al.moody.2020@gmail.com>
2020-09-10 00:31:38 +0300

    child

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 get_header(); ?>
 
+<?php
+function doubleMe ($x){
+echo $x * 2;
+}
+doubleMe(5);
+?>
+
 <div class="page-banner">
 <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri( 'images/library-hero.jpg' )?>);"></div>
 <div class="page-banner__content container t-center c-white">
--- page.php
+++ page.php
@@ -1,44 +1,62 @@
 <?php
 get_header();
-while (have_posts(  ))
-{
+while (have_posts()) {
 	the_post(); ?>
-	
+
 	<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri( '/images/ocean.jpg' ) ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-      <h1 class="page-banner__title"><?php the_title()?></h1>
-      <div class="page-banner__intro">
-        <p>DON'T FORGET TO REPLACE ME LATER</p>
-      </div>
-    </div>  
-  </div>
+		<div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+		<div class="page-banner__content container container--narrow">
+			<h1 class="page-banner__title"><?php the_title() ?></h1>
+			<div class="page-banner__intro">
+				<p>DON'T FORGET TO REPLACE ME LATER</p>
+			</div>
+		</div>
+	</div>
 
-  <div class="container container--narrow page-section">
+	<div class="container container--narrow page-section">
 
-	<?php ?>
-	
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title()?></span></p>
-    </div>
-    <!--
-    <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
-      <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
-      </ul>
-    </div>
--->
-    <div class="generic-content">
-	 <?PHP the_content();?>
-    </div>
+		<?php
 
-  </div>
+		$parentID = wp_get_post_parent_id(get_the_ID());
+		if ($parentID) { ?>
+			<div class="metabox metabox--position-up metabox--with-home-link">
+				<p><a class="metabox__blog-home-link" href="<?php echo get_permalink($parentID) ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parentID) ?></a> <span class="metabox__main"><?php the_title() ?></span></p>
+			</div>
+		<?php }
+		?>
+		<?php
+		$testArray = get_pages([
+			'child_of' => get_the_ID(),
+		]);
+		if ($parentID or $testArray) { ?>
+			<div class="page-links">
+				<h2 class="page-links__title"><a href="<?php echo get_permalink($parentID) ?>"><?php echo get_the_title($parentID) ?></a></h2>
+				<ul class="min-list">
+					<?php
+					if ($parentID) {
+						$findChildrenOf = $parentID;
+					} else {
+						$findChildrenOf = get_the_ID();
+					}
+					wp_list_pages([
+						'title_li' => null,
+						'child_of' => $findChildrenOf,
+						'sort_column' => 'menu_order',
+					]);
+					?>
+				</ul>
+			</div>
+		<?php } ?>
 
-	
-	
-	<?php 
+		<div class="generic-content">
+			<?PHP the_content(); ?>
+		</div>
+
+	</div>
+
+
+
+<?php
 }
 get_footer();
 ?>
